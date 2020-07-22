4319e84eb88c98524ca78825219d87770fff7937
plsankar1996 <plsankar1996@gmail.com>
2020-07-22 21:57:36 +0530

    Created Base theme & layout

--- app/src/main/java/com/ctrlbytes/deviceinfo/ui/main/MainActivity.java
+++ app/src/main/java/com/ctrlbytes/deviceinfo/ui/main/MainActivity.java
@@ -5,9 +5,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.ctrlbytes.deviceinfo.R;
+import com.google.android.material.appbar.AppBarLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.widget.NestedScrollView;
+import butterknife.BindDimen;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -15,14 +18,21 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.app_bar)
+    AppBarLayout appBar;
+    @BindView(R.id.scrollView)
+    NestedScrollView scrollView;
+
+    @BindDimen(R.dimen.app_bar_elevation)
+    int appBarElevation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
         setSupportActionBar(toolbar);
+        scrollView.setOnScrollChangeListener((NestedScrollView.OnScrollChangeListener) (v, sx, scrollY, osx, osy) -> appBar.setElevation(scrollY > 10 ? appBarElevation : 0));
     }
 
     @Override
