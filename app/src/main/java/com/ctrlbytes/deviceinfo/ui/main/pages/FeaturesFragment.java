9470f8db8954fb6423cd92c866bad59d5ec4cc32
plsankar1996 <plsankar1996@gmail.com>
2020-07-24 14:28:14 +0530

    Updated FeaturesFragment.java, info_listitem.xml

--- app/src/main/java/com/ctrlbytes/deviceinfo/ui/main/pages/FeaturesFragment.java
+++ app/src/main/java/com/ctrlbytes/deviceinfo/ui/main/pages/FeaturesFragment.java
@@ -27,7 +27,6 @@ package com.ctrlbytes.deviceinfo.ui.main.pages;
 
 import android.content.pm.FeatureInfo;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 
 import com.ctrlbytes.codekit.utils.YLog;
@@ -35,6 +34,44 @@ import com.ctrlbytes.deviceinfo.R;
 import com.ctrlbytes.deviceinfo.ui.base.InfoFragment;
 
 import androidx.annotation.Nullable;
+
+import static android.content.pm.PackageManager.FEATURE_AUDIO_LOW_LATENCY;
+import static android.content.pm.PackageManager.FEATURE_AUDIO_OUTPUT;
+import static android.content.pm.PackageManager.FEATURE_AUDIO_PRO;
+import static android.content.pm.PackageManager.FEATURE_BLUETOOTH;
+import static android.content.pm.PackageManager.FEATURE_BLUETOOTH_LE;
+import static android.content.pm.PackageManager.FEATURE_CAMERA;
+import static android.content.pm.PackageManager.FEATURE_CAMERA_AR;
+import static android.content.pm.PackageManager.FEATURE_CAMERA_AUTOFOCUS;
+import static android.content.pm.PackageManager.FEATURE_CAMERA_EXTERNAL;
+import static android.content.pm.PackageManager.FEATURE_CAMERA_FLASH;
+import static android.content.pm.PackageManager.FEATURE_CAMERA_FRONT;
+import static android.content.pm.PackageManager.FEATURE_CAMERA_LEVEL_FULL;
+import static android.content.pm.PackageManager.FEATURE_CONSUMER_IR;
+import static android.content.pm.PackageManager.FEATURE_ETHERNET;
+import static android.content.pm.PackageManager.FEATURE_GAMEPAD;
+import static android.content.pm.PackageManager.FEATURE_LOCATION;
+import static android.content.pm.PackageManager.FEATURE_LOCATION_GPS;
+import static android.content.pm.PackageManager.FEATURE_LOCATION_NETWORK;
+import static android.content.pm.PackageManager.FEATURE_MICROPHONE;
+import static android.content.pm.PackageManager.FEATURE_MIDI;
+import static android.content.pm.PackageManager.FEATURE_NFC;
+import static android.content.pm.PackageManager.FEATURE_NFC_BEAM;
+import static android.content.pm.PackageManager.FEATURE_PICTURE_IN_PICTURE;
+import static android.content.pm.PackageManager.FEATURE_PRINTING;
+import static android.content.pm.PackageManager.FEATURE_USB_ACCESSORY;
+import static android.content.pm.PackageManager.FEATURE_USB_HOST;
+import static android.content.pm.PackageManager.FEATURE_WIFI;
+import static android.content.pm.PackageManager.FEATURE_WIFI_AWARE;
+import static android.content.pm.PackageManager.FEATURE_WIFI_DIRECT;
+import static android.content.pm.PackageManager.FEATURE_WIFI_PASSPOINT;
+import static android.os.Build.VERSION.SDK_INT;
+import static android.os.Build.VERSION_CODES.M;
+import static android.os.Build.VERSION_CODES.N;
+import static android.os.Build.VERSION_CODES.O;
+import static android.os.Build.VERSION_CODES.O_MR1;
+import static android.os.Build.VERSION_CODES.P;
+import static android.os.Build.VERSION_CODES.Q;
 
 public class FeaturesFragment extends InfoFragment {
 
@@ -48,51 +85,44 @@ public class FeaturesFragment extends InfoFragment {
 
         mPackageManager = requireContext().getPackageManager();
 
-        add(R.string.features_wifi, checkFeature(PackageManager.FEATURE_WIFI));
-        add(R.string.features_wifi_direct, checkFeature(PackageManager.FEATURE_WIFI_DIRECT));
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            add(R.string.features_wifi_aware, checkFeature(PackageManager.FEATURE_WIFI_AWARE));
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
-            add(R.string.features_wifi_passpoint, checkFeature(PackageManager.FEATURE_WIFI_PASSPOINT));
-        }
-        add(R.string.features_bluetooth, checkFeature(PackageManager.FEATURE_BLUETOOTH));
-        add(R.string.features_bluetooth_le, checkFeature(PackageManager.FEATURE_BLUETOOTH_LE));
+        add(R.string.features_wifi, checkFeature(FEATURE_WIFI));
+        add(R.string.features_wifi_direct, checkFeature(FEATURE_WIFI_DIRECT));
+        add(R.string.features_wifi_aware, SDK_INT >= O ? checkFeature(FEATURE_WIFI_AWARE) : R.string.not_supported);
+        add(R.string.features_wifi_passpoint, SDK_INT >= O_MR1 ? checkFeature(FEATURE_WIFI_PASSPOINT) : R.string.not_supported);
+        add(R.string.features_bluetooth, checkFeature(FEATURE_BLUETOOTH));
+        add(R.string.features_bluetooth_le, checkFeature(FEATURE_BLUETOOTH_LE));
 
-        add(R.string.features_camera, checkFeature(PackageManager.FEATURE_CAMERA));
-        add(R.string.features_camera_autofocus, checkFeature(PackageManager.FEATURE_CAMERA_AUTOFOCUS));
-        add(R.string.features_camera_external, checkFeature(PackageManager.FEATURE_CAMERA_EXTERNAL));
-        add(R.string.features_camera_flash, checkFeature(PackageManager.FEATURE_CAMERA_FLASH));
-        add(R.string.features_camera_level, checkFeature(PackageManager.FEATURE_CAMERA_LEVEL_FULL));
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            add(R.string.features_camera_ar, checkFeature(PackageManager.FEATURE_CAMERA_AR));
-        }
+        add(R.string.features_camera, checkFeature(FEATURE_CAMERA));
+        add(R.string.features_camera_front, checkFeature(FEATURE_CAMERA_FRONT));
+        add(R.string.features_camera_autofocus, checkFeature(FEATURE_CAMERA_AUTOFOCUS));
+        add(R.string.features_camera_external, checkFeature(FEATURE_CAMERA_EXTERNAL));
+        add(R.string.features_camera_flash, checkFeature(FEATURE_CAMERA_FLASH));
+        add(R.string.features_camera_level, checkFeature(FEATURE_CAMERA_LEVEL_FULL));
+        add(R.string.features_camera_ar, SDK_INT >= P ? checkFeature(FEATURE_CAMERA_AR) : R.string.not_supported);
 
-        add(R.string.feature_consumer_ir, checkFeature(PackageManager.FEATURE_CONSUMER_IR));
+        add(R.string.feature_consumer_ir, checkFeature(FEATURE_CONSUMER_IR));
 
-        add(R.string.feature_location, checkFeature(PackageManager.FEATURE_LOCATION));
-        add(R.string.feature_location_gps, checkFeature(PackageManager.FEATURE_LOCATION_GPS));
-        add(R.string.feature_location_network, checkFeature(PackageManager.FEATURE_LOCATION_NETWORK));
+        add(R.string.feature_location, checkFeature(FEATURE_LOCATION));
+        add(R.string.feature_location_gps, checkFeature(FEATURE_LOCATION_GPS));
+        add(R.string.feature_location_network, checkFeature(FEATURE_LOCATION_NETWORK));
 
-        add(R.string.feature_microphone, checkFeature(PackageManager.FEATURE_MICROPHONE));
+        add(R.string.features_audio_output, checkFeature(FEATURE_AUDIO_OUTPUT));
+        add(R.string.features_audio_low_latency, checkFeature(FEATURE_AUDIO_LOW_LATENCY));
+        add(R.string.features_audio_pro, SDK_INT >= M ? checkFeature(FEATURE_AUDIO_PRO) : R.string.not_supported);
+        add(R.string.feature_microphone, checkFeature(FEATURE_MICROPHONE));
 
-        add(R.string.feature_nfc, checkFeature(PackageManager.FEATURE_NFC));
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            add(R.string.feature_nfc_beam, checkFeature(PackageManager.FEATURE_NFC_BEAM));
-        }
+        add(R.string.feature_nfc, checkFeature(FEATURE_NFC));
+        add(R.string.feature_nfc_beam, SDK_INT >= Q ? checkFeature(FEATURE_NFC_BEAM) : R.string.not_supported);
 
-        add(R.string.feature_usb_host, checkFeature(PackageManager.FEATURE_USB_HOST));
-        add(R.string.feature_usb_accesseries, checkFeature(PackageManager.FEATURE_USB_ACCESSORY));
+        add(R.string.feature_usb_host, checkFeature(FEATURE_USB_HOST));
+        add(R.string.feature_usb_accesseries, checkFeature(FEATURE_USB_ACCESSORY));
 
-        add(R.string.feature_print, checkFeature(PackageManager.FEATURE_PRINTING));
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            add(R.string.feature_pip, checkFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE));
-            add(R.string.feature_ethernet, checkFeature(PackageManager.FEATURE_ETHERNET));
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            add(R.string.feature_midi, checkFeature(PackageManager.FEATURE_MIDI));
-        }
-        add(R.string.feature_gamepad, checkFeature(PackageManager.FEATURE_GAMEPAD));
+        add(R.string.feature_print, checkFeature(FEATURE_PRINTING));
+        add(R.string.feature_pip, SDK_INT >= N ? checkFeature(FEATURE_PICTURE_IN_PICTURE) : R.string.not_supported);
+        add(R.string.feature_ethernet, SDK_INT >= N ? checkFeature(FEATURE_ETHERNET) : R.string.not_supported);
+        add(R.string.feature_midi, SDK_INT >= M ? checkFeature(FEATURE_MIDI) : R.string.not_supported);
+
+        add(R.string.feature_gamepad, checkFeature(FEATURE_GAMEPAD));
 
         FeatureInfo[] features = mPackageManager.getSystemAvailableFeatures();
         for (FeatureInfo info : features) {
@@ -103,6 +133,6 @@ public class FeaturesFragment extends InfoFragment {
     }
 
     private int checkFeature(String feature) {
-        return mPackageManager.hasSystemFeature(feature) ? R.string.available : R.string.unavailable;
+        return mPackageManager.hasSystemFeature(feature) ? R.string.available : R.string.not_supported;
     }
 }
